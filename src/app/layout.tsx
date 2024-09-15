b02ef9516163e34f0c3487538e83942ea302cd42
Emanuel Särnhammar <emanuel@quaqqer.com>
2024-09-15 22:44:38 +0200

    Set background color of html body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import type { Metadata } from "next";
 import { Roboto_Slab } from "next/font/google";
 
@@ -20,8 +21,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={robotoSlab.className}>
-        <div className={"min-h-screen bg-gray-800 text-gray-500"}>
+      <body className={clsx(robotoSlab.className, "bg-gray-900 text-gray-500")}>
+        <div className="min-h-screen">
           <Navbar />
 
           <div className="mx-auto max-w-screen-lg px-4 sm:px-6 lg:px-8">
