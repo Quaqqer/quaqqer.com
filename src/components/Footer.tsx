8626c9d169c84d21f8d924d6a174b172c78f37fc
Emanuel Särnhammar <emanuel@quaqqer.com>
2025-09-02 15:52:40 +0200

    chore: update copyright notice

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer(): JSX.Element {
   return (
     <footer className="bg-gray-900 px-5 text-sm text-gray-400">
-      <div className="mx-auto max-w-xl py-8">Copyright © 2024 Quaqqer</div>
+      <div className="mx-auto max-w-xl py-8">Copyright © 2025 Quaqqer</div>
     </footer>
   );
 }
